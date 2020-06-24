f74ec60980a14c16cd8450e80d95f604758722a4
zoeng <zoe.ng@utexas.edu>
2020-06-23 20:40:12 -0500

    Updates

---
+++ precache-manifest.3644de603047f2c24321c8084c71ff06.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a14c1d0bff60869500efdfb96e7768a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a7e34b444e0b12c4e72",
+    "url": "/static/css/main.9c672774.chunk.css"
+  },
+  {
+    "revision": "bb3f7391a92acd483e43",
+    "url": "/static/js/2.aec824c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.aec824c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a7e34b444e0b12c4e72",
+    "url": "/static/js/main.af8bea4c.chunk.js"
+  },
+  {
+    "revision": "c815442ab336b747f527",
+    "url": "/static/js/runtime-main.94a6af97.js"
+  },
+  {
+    "revision": "64eadb202ea41ebff13e8bc66ebb0210",
+    "url": "/static/media/AirThoughts.64eadb20.png"
+  },
+  {
+    "revision": "aaacd72552a21aa10b676ad158ad4c24",
+    "url": "/static/media/Calculator.aaacd725.png"
+  },
+  {
+    "revision": "27dc34a0558c1da5a4fcc8ea08eebdc2",
+    "url": "/static/media/ChatProgram.27dc34a0.png"
+  },
+  {
+    "revision": "0e12fcdd94bb3b6884c7718c5ef98655",
+    "url": "/static/media/DSC_0174.0e12fcdd.jpg"
+  },
+  {
+    "revision": "86883a201183524d04ef4cf54cc7412b",
+    "url": "/static/media/Hangman.86883a20.png"
+  },
+  {
+    "revision": "42ecf29c8602a463ffa22467bf34fc5b",
+    "url": "/static/media/Mastermind.42ecf29c.png"
+  },
+  {
+    "revision": "ca20e5994c707dff3eba134b8288952f",
+    "url": "/static/media/Nim.ca20e599.png"
+  },
+  {
+    "revision": "7a591f25b58bbc8a6d735f046b73afd2",
+    "url": "/static/media/stellarspine.7a591f25.png"
+  }
+]);
